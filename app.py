174f25565bdbbd0ea5e080d5e3c8cb400a465d9b
unknown <laidstall@gmail.com>
2020-06-20 00:56:39 +0100

    mywebsite

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/<string:page_name>')
+def html_pages(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt','a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('./web-server/database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form', methods=['POST','GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Error, Try again'
+
+if __name__=='__main__':
+    app.run(debug=True)
